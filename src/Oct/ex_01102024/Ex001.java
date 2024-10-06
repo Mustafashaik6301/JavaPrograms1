321ce28088314247681c80d5e188ce7f56c1e05c
mshaik45 <mmsk3213@gmail.com>
2024-10-06 13:20:15 +0530

    My Files

---
+++ src/Oct/ex_01102024/Ex001.java
@@ -0,0 +1,24 @@
+package Oct.ex_01102024;
+
+public class Ex001 {
+    public static void main(String[] args) {
+        System.out.println("10X1=10");
+        System.out.println("10X2=20");
+        System.out.println("10X3=30");
+        System.out.println("10X4=40");
+        System.out.println("10X5=50");
+        System.out.println("10X6=60");
+        System.out.println("10X7=70");
+        System.out.println("10X8=80");
+        System.out.println("10X9=90");
+        System.out.println("10X10=100");
+
+
+
+    }
+
+
+
+
+
+}
