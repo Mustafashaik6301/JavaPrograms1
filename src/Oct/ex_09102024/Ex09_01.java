11bfae880b83485785fcfe28f61af1aeb7261c77
mshaik45 <mmsk3213@gmail.com>
2024-10-10 09:16:10 +0530

    09 Files

---
+++ src/Oct/ex_09102024/Ex09_01.java
@@ -0,0 +1,21 @@
+package Oct.ex_09102024;
+
+public class Ex09_01 {
+
+
+    public static void main(String[] args) {
+
+        int i = 2;
+        byte b = 127;
+        //shot s = 45;
+        long l = 456345678l;
+
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(l);
+        //System.out.println(s);
+
+
+    }
+
+}
---
+++ src/Oct/ex_09102024/Ex09_02.java
@@ -0,0 +1,14 @@
+package Oct.ex_09102024;
+
+public class Ex09_02 {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+        System.out.println(a);
+        System.out.print("a"+"b");
+        System.out.println("a"+"b");
+       // System.out.printf(char a is, c);
+
+    }
+}
