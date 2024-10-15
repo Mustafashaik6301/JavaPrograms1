1dd07ab93ba0a91a953cfc461753d453ae2e3ef0
mshaik45 <mmsk3213@gmail.com>
2024-10-15 18:26:42 +0530

    Hi

---
+++ src/Oct/ex_15102024_Increments_Decrements/Ex011.java
@@ -0,0 +1,13 @@
+package Oct.ex_15102024_Increments_Decrements;
+
+public class Ex011 {
+    public static void main(String[] args) {
+        int a = 1, b = a++, c = a++, d = ++a, e = --a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a+b+c);
+
+
+    }
+}
