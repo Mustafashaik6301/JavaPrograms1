c4641b7eb37e4312065b51f59ce816c0f07468aa
mshaik45 <mmsk3213@gmail.com>
2024-10-15 12:19:01 +0530

    14 oct

---
+++ src/Oct/ex_14102024/Ex011.java
@@ -0,0 +1,12 @@
+package Oct.ex_14102024;
+
+public class Ex011 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = a++;
+        int c = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
