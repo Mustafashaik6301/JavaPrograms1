1cb1092c0a684e5411467d941834dcf5d31281c3
mshaik45 <mmsk3213@gmail.com>
2024-10-15 19:19:03 +0530

    Hi

---
+++ src/Oct/ex_15102024_Increments_Decrements/Ex014.java
@@ -0,0 +1,22 @@
+package Oct.ex_15102024_Increments_Decrements;
+
+public class Ex014 {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = ++a;
+        int c = ++a;
+        int d = a++;
+        int e = ++a;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        int i = 6, f = ++i, g = i++, h = ++i, j = i++;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(j);
+    }
+}
