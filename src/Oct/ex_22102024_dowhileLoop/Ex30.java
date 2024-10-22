1aa9f0c440d44298c294a73cbb5b81bf8720be90
mshaik45 <mmsk3213@gmail.com>
2024-10-22 15:03:04 +0530

    Hi

---
+++ src/Oct/ex_22102024_dowhileLoop/Ex30.java
@@ -0,0 +1,16 @@
+package Oct.ex_22102024_dowhileLoop;
+
+
+
+public class Ex30 {
+    public static void main(String[] args) {
+        int i = 0;
+        do
+
+            System.out.println(i);
+            while (i <= 5);
+            i++;
+
+
+    }
+}
